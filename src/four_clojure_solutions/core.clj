4955d043fc4a8911bf693e75de62082e3c86a05f
mmallya775 <mmallya775@gmail.com>
2025-05-17 00:20:11 +0200

    solved till 38 16May2025

--- src/four_clojure_solutions/core.clj
+++ src/four_clojure_solutions/core.clj
@@ -373,3 +373,134 @@
 
 
 
+;; Problem 31, Pack a Sequence
+;;
+;; Write a function which packs consecutive duplicates into sub-lists.
+;;
+;;(= (__ [1 1 2 1 1 1 3 3]) '((1 1) (2) (1 1 1) (3 3)))
+;;(= (__ [:a :a :b :b :c]) '((:a :a) (:b :b) (:c)))
+;;(= (__ [[1 2] [1 2] [3 4]]) '(([1 2] [1 2]) ([3 4])))
+
+
+(partition-by (fn [x] x) [1 1 2 1 1 1 3 3])
+
+
+
+
+
+;; Problem 32, Duplicate a Sequence
+;;
+;; Write a function which duplicates each element of a sequence.
+;;
+;;(= (__ [1 2 3]) '(1 1 2 2 3 3))
+;;(= (__ [:a :a :b :b]) '(:a :a :a :a :b :b :b :b))
+;;(= (__ [[1 2] [3 4]]) '([1 2] [1 2] [3 4] [3 4]))
+;;(= (__ [44 33]) [44 44 33 33])
+
+
+(mapcat (fn [x]
+          (repeat 2 x))
+        [[1 2] [3 4]])
+
+
+
+
+;; Problem 33, Replicate a Sequence
+;;
+;; Write a function which replicates each element of a sequence a variable number of times.
+;;
+;;(= (__ [1 2 3] 2) '(1 1 2 2 3 3))
+;;(= (__ [:a :b] 4) '(:a :a :a :a :b :b :b :b))
+;;(= (__ [4 5 6] 1) '(4 5 6))
+;;(= (__ [[1 2] [3 4]] 2) '([1 2] [1 2] [3 4] [3 4]))
+;;(= (__ [44 33] 2) [44 44 33 33])
+
+((fn [c n]
+   (mapcat
+     #(repeat n %) c))
+ [[1 2] [3 4]] 2)
+
+
+
+
+
+
+;; Problem 34, Implement range
+;;
+;; Write a function which creates a list of all integers in a given range.
+;;
+;;(= (__ 1 4) '(1 2 3))
+;;(= (__ -2 2) '(-2 -1 0 1))
+;;(= (__ 5 8) '(5 6 7))
+
+((fn [s e]
+   (take (- e s) (iterate inc s))) -2 2)
+
+;without using the iterate method
+((fn [s e] (take (- e s) ((fn lzs [b]
+                            (lazy-seq
+                              (cons b (lzs (inc b))))) s))) 1 4)
+
+
+
+
+;; Problem 35, Local bindings
+;;
+;; Clojure lets you give local names to values using the special let-form.
+;;
+;;(= __ (let [x 5] (+ 2 x)))
+;;(= __ (let [x 3, y 10] (- y x)))
+;;(= __ (let [x 21] (let [y 3] (/ x y))))
+
+
+(= 7 (let [x 5] (+ 2 x)))
+
+
+
+
+;; Problem 36, Let it Be
+;;
+;; Can you bind x, y, and z so that these are all true?
+;;
+;;(= 10 (let __ (+ x y)))
+;;
+;;(= 4 (let __ (+ y z)))
+;;
+;;(= 1 (let __ z))
+
+
+
+(= 10 (let [x 7
+            y 3
+            z 1]
+        (+ x y)))
+
+
+
+
+
+;; Problem 37, Regular Expressions
+;;
+;; (= __ (apply str (re-seq #"[A-Z]+" "bA1B3Ce ")))
+
+
+(= "ABC" (apply str (re-seq #"[A-Z]+" "bA1B3Ce ")))
+
+
+
+
+
+
+;; Problem 38, Maximum value
+;;
+;; Write a function which takes a variable number of parameters and returns the maximum value.
+;;
+;;(= (__ 1 8 3 4) 8)
+;;(= (__ 30 20) 30)
+;;(= (__ 45 67 11) 67)
+
+
+((fn [& nums]
+   (->> (seq nums)
+        sort
+        last)) 30 20)
